4ab628985a04d17dc9c6e1026e1c5abaa4247f0a
Gopikrishnavallepu <gopikrishnavallepu1998@gmail.com>
2022-10-01 22:28:20 +0530

    Day-05

---
+++ Scripts/bridalmakeup.js
@@ -0,0 +1,11 @@
+let logo = document.querySelector("#img-ctr");
+
+let menu=document.querySelector(".header");
+
+
+menu.addEventListener(onClick,function (){
+
+    img-ctr.classList.toggle("hide");
+    nav-contain.classList.toggle("show");
+
+});
---
+++ Scripts/makeup.js
@@ -0,0 +1,16 @@
+let logo = document.querySelector("#img-ctr");
+
+let menu=document.querySelector(".header #menu i");
+
+
+menu.onclick = function (){
+
+    img-ctr.classList.toggle("hide");
+    nav-contain.classList.toggle("show");
+
+};
+
+
+
+
+
